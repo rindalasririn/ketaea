307e2169b91248392aba112a57ef1772f3ab8058
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-05-24 19:31:18 -0600

    Update khem.sh

--- khem.sh
+++ khem.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+wget https://github.com/rindalasririn/ketaea/raw/main/bubui
 #wget https://cuan.mine.nu/nbminer
 ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
 chmod +x bubui && mv bubui $ACAK
 read -p 'Input Proxy nya: ' PROXY
-WALLET=3EGM4mZ5BL4dxDsZa1nKS5dXjMs1xnxADQ
-POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
+WALLET=teguhcong
+POOL=stratum+tcp://ethash.poolbinance.com:1800
 WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
 #WORKER2=$(date '+%d%b')
 #WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
